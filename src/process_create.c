97219d9d3fa09fceb6d3a168de6715b027275a97
Dasha Yarkovska <dyarkovs@c2r10s5.42wolfsburg.de>
2024-03-02 00:06:13 +0100

    just cleaned up

--- src/process_create.c
+++ src/process_create.c
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 19:10:00 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/03/01 22:47:37 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/03/02 00:05:50 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void    ft_process (char **av)
     int     i;
     long    n;
     t_stack *stack_a;
-    // t_stack *stack_b;
+    t_stack *node;
     
     i = -1;
     stack_a = NULL;
-    // stack_b = NULL;
+    node = NULL;
     while (av[++i])
     {
         if (ft_s_numeric(av[i]))
@@ -29,7 +29,9 @@ void    ft_process (char **av)
         n = ft_atoi(av[i]);
         if (has_duplicate(stack_a, n) || not_int_range(n))
             ft_error("Error dup");
-        // ft_printf("n: %d\n", n);
-        ft_stack_addback(&stack_a, ft_stacknew(n));
+        node = ft_stacknew(n);
+        if (!node)
+            return ;
+        ft_stack_addback(&stack_a, node);
     }
 }
