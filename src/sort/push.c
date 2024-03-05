295ce867b8c1d92257328b087b016831dd13bffe
Dasha Yarkovska <dyarkovs@c1s3d14.42wolfsburg.de>
2024-03-05 17:32:20 +0100

    test

--- src/sort/push.c
+++ src/sort/push.c
@@ -6,12 +6,11 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 17:04:06 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/03/05 17:26:45 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/03/05 17:32:11 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
-
 
 // int push (t_stack **stack)
 // {
