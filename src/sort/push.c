166ad80fb886f0f97d1b9790648b2106469b1cc0
Dasha Yarkovska <dyarkovs@c1s3d14.42wolfsburg.de>
2024-03-05 21:41:43 +0100

    save, operations done

--- src/sort/push.c
+++ src/sort/push.c
@@ -6,34 +6,36 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 17:04:06 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/03/05 17:32:11 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/03/05 20:51:54 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-// int push (t_stack **stack)
-// {
-    
-//     if ((*stack) == NULL || (*stack)->next == NULL)
-//         return (0);
-    
-
-
-
-//     return (1);
-// }
-
-void    pa(t_stack **stack_a, t_stack **stack_b)
+int	push(t_stack **from, t_stack **to)
 {
-    if(swap(stack_a))
-        ft_printf ("sa\n");
+	t_stack	*head_to;
+
+	if ((*from) == NULL)
+		return (0);
+	head_to = *to;
+	*to = *from;
+	if ((*from)->next != NULL)
+		*from = (*from)->next;
+	(*to)->next = head_to;
+	return (1);
 }
 
-void    pb(t_stack **stack_a, t_stack **stack_b)
+void	pa(t_stack **stack_a, t_stack **stack_b)
 {
-    if(swap(stack_b))
-        ft_printf ("sb\n");
+	if (push (stack_b, stack_a))
+		ft_printf ("sa\n");
+}
+
+void	pb(t_stack **stack_a, t_stack **stack_b)
+{
+	if (push (stack_a, stack_b))
+		ft_printf ("sb\n");
 }
 
 
--- src/utils/list_utils.c
+++ src/utils/list_utils.c
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 19:44:47 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/03/05 14:24:18 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/03/05 17:55:30 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,57 @@
 
 t_stack *ft_stacknew (int n)
 {
-    t_stack *node;
+	t_stack *node;
 
-    node = (t_stack *)malloc(sizeof(t_stack));
-    if (!node)
-        return (NULL);
-    node->n = n;
-    node->next = NULL;
-    // ft_printf("new node: %d\n", node->n);
-    return (node);
+	node = (t_stack *)malloc(sizeof(t_stack));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	// ft_printf("new node: %d\n", node->n);
+	return (node);
 }
 
 t_stack *ft_stacklast(t_stack *stack)
 {
-    while (stack && stack->next)
-        stack = stack->next;
-    return (stack);
+	while (stack && stack->next)
+		stack = stack->next;
+	return (stack);
 }
 
 void    ft_stack_addback(t_stack **stack, t_stack *new)
 {
-    t_stack *last;
+	t_stack *last;
 
-    if (!*stack)
-        *stack = new;
-    else
-    {
-        last = ft_stacklast(*stack);
-        last->next = new;
-    }
+	if (!*stack)
+		*stack = new;
+	else
+	{
+		last = ft_stacklast(*stack);
+		last->next = new;
+	}
 }
 
 void    ft_del_stacknode(t_stack *node)
 {
-    if (!node)
-        return ;
-    // node->n = 0;
-    // node->next = NULL;
-    free(node);
+	if (!node)
+		return ;
+	// node->n = 0;
+	// node->next = NULL;
+	free(node);
 }
 
 void    ft_stackclear(t_stack **stack)
 {
-    t_stack *tmp;
+	t_stack *tmp;
 
-    while (*stack)
-    {
-        ft_printf ("clear stack | ");
-        tmp = (*stack)->next;
-        free(*stack);
-        // ft_del_stacknode(*stack);
-        *stack = tmp;
-    }
-    *stack = NULL;
+	while (*stack)
+	{
+		ft_printf ("clear stack | ");
+		tmp = (*stack)->next;
+		free(*stack);
+		// ft_del_stacknode(*stack);
+		*stack = tmp;
+	}
+	*stack = NULL;
 }
