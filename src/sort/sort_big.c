0c0a8fefe098d4ec3d1774d48a012fedaf5b30c1
Dasha Yarkovska <dyarkovs@c2r8s8.42wolfsburg.de>
2024-03-16 20:38:19 +0100

    cleaned comments in sort_big

--- src/sort/sort_big.c
+++ src/sort/sort_big.c
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:24:16 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/03/16 03:55:29 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/03/16 20:38:05 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ void	quick_sort(t_stack **stack_a, t_stack **stack_b)
 
 void	do_dbl_rot_amnt(int amnt, t_stack *cheapest, t_stack **a, t_stack **b)
 {
-		// ft_printf("%si:%d, t:%d%s\n", MAGENTA, cheapest->i, cheapest->target->i, RESET_COLOR);
+	// ft_printf("%si:%d, t:%d%s\n", MAGENTA, cheapest->i, cheapest->target->i, RESET_COLOR);
 	if (amnt > 0)
 	{
 		while (cheapest->i && cheapest->target->i)
@@ -98,9 +98,7 @@ void	do_dbl_rot_amnt(int amnt, t_stack *cheapest, t_stack **a, t_stack **b)
 			cheapest->target->i++;
 			rrr(a, b);
 		}
-		// ft_printf("%sA: %d, B: %d%s\n",YELLOW, (*a)->n, (*b)->n, RESET_COLOR);
 	}
-	// ft_printf("%si:%d, t:%d%s\n", MAGENTA, cheapest->i, cheapest->target->i, RESET_COLOR);
 }
 
 void	do_rot_amnt(t_stack *node, t_stack **stack_a, t_stack **stack_b)
@@ -136,32 +134,12 @@ void	turk_sort(t_stack **stack_a, t_stack **stack_b)
 	t_stack	*cheapest;
 	int		move_both;
 
-	// head_a = *stack_a;
-	// head_b = *stack_b;
 	cheapest = NULL;
-	// ft_printf("TURK SORT\n--------------------\n");
-	// ft_printf("STACK A\n");
-	// while (head_a)
-	// {
-	// 	ft_printf("%d,", (head_a)->n);
-	// 	head_a = (head_a)->next;
-	// }
-	// ft_printf("\n---------------\nSTACK B\n");
-	// while (head_b)
-	// {
-	// 	ft_printf("%d,", (head_b)->n);
-	// 	head_b = (head_b)->next;
-
-	// }
-	// ft_printf("\n--------------------\n");
 	set_target_node(*stack_a, *stack_b);
-	// ft_printf("\n COST STACK A\n");
 	set_cost(*stack_a);
-	// ft_printf("\n COST STACK B\n");
 	set_cost(*stack_b);
 	cheapest = final_cost(*stack_b);
 	move_both = position_both(cheapest);
-	// ft_printf("move_both: %d\n", move_both);
 	if (move_both)
 	{
 		// if ((move_both < 0 && (cheapest->i < cheapest->target->i))
@@ -179,53 +157,20 @@ void	turk_sort(t_stack **stack_a, t_stack **stack_b)
 		// }
 		do_dbl_rot_amnt(move_both, cheapest, stack_a, stack_b);
 	}
-	//*do one func, but pass cheapest first, next call - pass cheapest->target
-	//!use this same func to rot stack_a until finish at the end
 	do_rot_amnt(cheapest, stack_a, stack_b);
 	pa(stack_a, stack_b);
-	// ft_printf("STACK A\n");
-	// head_a = *stack_a;
-	// head_b = *stack_b;
-	// while (head_a)
-	// {
-	// 	ft_printf("%d,", (head_a)->n);
-	// 	head_a = (head_a)->next;
-	// }
-	// ft_printf("\n---------------\nSTACK B\n");
-	// while (head_b)
-	// {
-	// 	ft_printf("%d,", (head_b)->n);
-	// 	head_b = (head_b)->next;
-	// }
 	//if i can rr or rrr both stacks, then do this
 	//afther, minus the cost (i),
 	//move the leftover moves for one of the stacks
 	//pa
 	//--------------------redo the sort while there is smth in stack_b
 }
-	// ft_printf("cheapest node: %d, target: %d\n",
-	// cheapest_node->n, cheapest_node->target->n);
-
-// void	refresh_data(t_stack **stack_a, t_stack **stack_b)
-// {
-// 	*stack_a = NULL;
-// 	*stack_b = NULL;
-// 	ft_printf("refresh data\n");
-// }
 
 void	sort_big(t_stack **stack_a, t_stack **stack_b)
 {
-	// int	max_idx;
-	// t_stack	*head_a;
-	// t_stack	*head_b;
-
 	quick_sort(stack_a, stack_b);
-	// sort3(stack_a);
-	// ft_printf("b len: %d\n", ft_stacklen(*stack_b));
 	while (ft_stacklen(*stack_b) > 0)
 		turk_sort(stack_a, stack_b);
-	// ft_printf("%sAFTER TURK%s\n", YELLOW, RESET_COLOR);
-	// if the n value is after mid => ra, else => rra while is not sorted
 	if (!is_sorted(*stack_a))
 	{
 		// set_cost(*stack_a);
@@ -235,28 +180,6 @@ void	sort_big(t_stack **stack_a, t_stack **stack_b)
 		// 	/* code */
 		// }
 		set_cost(*stack_a);
-		// head_a = *stack_a;
-		// head_b = *stack_b;
-		// ft_printf("STACK A\n");
-		// while (head_a)
-		// {
-		// 	ft_printf("%d(p %d), ", (head_a)->n, (head_a)->i);
-		// 	head_a = (head_a)->next;
-		// }
-		// ft_printf("\n---------------\nSTACK B\n");
-		// while (head_b)
-		// {
-		// 	ft_printf("%d,", (head_b)->n);
-		// 	head_b = (head_b)->next;
-		// }
 		do_rot_amnt(find_max(*stack_a)->next, stack_a, stack_b);
-
-		// ra(stack_a);
 	}
-	// while (*stack_a)
-	// {
-	// 	ft_printf("%d,", (*stack_a)->n);
-	// 	*stack_a = (*stack_a)->next;
-	// }
-	// refresh_data(stack_a, stack_b);
 }
